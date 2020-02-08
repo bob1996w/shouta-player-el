91d53a5c44576703cbeb6babb4a3e598490fab1e
bob1996w <bob1996w@gmail.com>
2020-02-08 21:58:48 +0800

    implement nowPlayingInfo

--- src/client/components/mainViewport/nowPlayingView/NowPlayingInfo.tsx
+++ src/client/components/mainViewport/nowPlayingView/NowPlayingInfo.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
+import * as Path from 'path';
+import { EAppIpcAction } from '../../../../shared/AppIpc/EAppIpcAction';
 
 export function NowPlayingInfo(props: any) {
+    const [audioTitle, setAudioTitle] = React.useState('');
+    const [audioArtist, setAudioArtist] = React.useState('');
+    const [audioAlbum, setAudioAlbum] = React.useState('');
+
+    React.useEffect(() => {
+        // run once on first render
+        props.ipc.on('Audio', EAppIpcAction.Response, (request: string, data: any) => {
+            if (request === 'currentAudioData') {
+                setAudioTitle(data.metadata.title? data.metadata.title: Path.basename(data.path));
+                setAudioArtist(data.metadata.artist? data.metadata.artist: '');
+                setAudioAlbum(data.metadata.album? data.metadata.album: '');
+            }
+        });
+    }, [])
+
     return (
         <div id="nowPlayingInfo">
-            <p>This is a song.</p>
-            <p>This is an artist.</p>
-            <p>This is an album.</p>
+            <p>{audioTitle}</p>
+            <p>{audioArtist}</p>
+            <p>{audioAlbum}</p>
         </div>
     );
 }
